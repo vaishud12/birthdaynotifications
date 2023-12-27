f8d50053918464ee96bb8d64879a4f00b3cd475c
Vaishnavi Devardekar <vaishuu2397@gmail.com>
2023-12-27 11:27:51 +0530

    finalbd

--- src/Birthday/Data.js
+++ src/Birthday/Data.js
@@ -1,24 +1,31 @@
 const Data = [
     {
         "name":"Sha Rukh Khan",
-        "age":26,
+        "age":58,
         "image":"https://in.bmscdn.com/iedb/artist/images/website/poster/large/shah-rukh-khan-2092-12-09-2017-02-10-43.jpg"
 
     },
     {
-        "name":"Sha Rukh Khan",
-        "age":26,
-        "image":"https://in.bmscdn.com/iedb/artist/images/website/poster/large/shah-rukh-khan-2092-12-09-2017-02-10-43.jpg"
+        "name":"Katrina Kaif",
+        "age":40,
+        "image":"https://www.thenews.com.pk/assets/uploads/updates/2023-02-22/1043191_4033771_kki_updates.jpg"
     },
     {
-        "name":"Sha Rukh Khan",
-        "age":26,
-        "image":"https://in.bmscdn.com/iedb/artist/images/website/poster/large/shah-rukh-khan-2092-12-09-2017-02-10-43.jpg"
+        "name":" Hrithik Roshan",
+        "age":49,
+        "image":"https://images1.dnaindia.com/images/DNA-EN/900x1600/2023/12/20/1703046061279_MV5BMTkxMDEzNDgxMV5BMl5BanBnXkFtZTgwMTgzODgyNDM.V1.jpg"
     },
     {
-        "name":"Sha Rukh Khan",
-        "age":26,
-        "image":"https://in.bmscdn.com/iedb/artist/images/website/poster/large/shah-rukh-khan-2092-12-09-2017-02-10-43.jpg"
-    }
+        "name":"Akshay Kumar",
+        "age":56,
+        "image":"https://www.masala.com/cloud/2023/11/27/tHzGHYqE-Screen-Shot-2023-11-27-at-7.24.23-PM-1200x799.png"
+    },
+    {
+        "name":"Alia Bhatt",
+        "age":30,
+        "image":"https://static.independent.co.uk/2022/06/29/10/GettyImages-1370877934.jpeg"
+    },
+
+    
 ]
 export default Data
